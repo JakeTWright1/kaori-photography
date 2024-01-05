baf99cfa9afb5df281c7f3b012e754c9d87fcc56
Jake Wright <jaketwright0@gmail.com>
2024-01-05 17:59:44 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
+  get "/about", to: "pages#about"
+  get "/gallery", to: "pages#gallery"
+  get "/contact", to: "pages#contact"
 end
